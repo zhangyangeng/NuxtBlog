76aed6c763d4961db16e720ab6b715e029c5c1c3
Wrysmile <15035374951@126.com>
2024-11-20 23:53:43 +0800

    chore: 更新Nuxt版本，修改开发模式端口

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    devServer: {
+        port: 5173,
+    },
     modules: [
         [
             '@nuxtjs/i18n',
