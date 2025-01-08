d59597421d8c7f02efb2726dda28cdd06e46ef00
Wrysmile <15035374951@126.com>
2025-01-08 22:23:08 +0800

    feat: 封装文章标签组件

---
+++ components/common/Tag.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="tag-container">
+        <el-divider content-position="center"></el-divider>
+        <div class="tag-list">
+            <el-tag v-for="item in 2" :key="item" class="m-1" round @click="jumpToTagDetail('大模型')"># 大模型{{ item }}</el-tag>
+        </div>
+    </div>
+</template>
+
+<!-- 文章标签 -->
+<script setup lang="ts">
+/**
+ * 跳转到标签详情页
+ * @param {string} name Tag 名称
+ */
+async function jumpToTagDetail(name: string): Promise<void> {
+    await navigateTo(`/tags/${name}`);
+}
+</script>
+
+<style scoped lang="scss">
+.tag-container {
+    width: 100%;
+    .tag-list {
+        text-align: center;
+        .el-tag {
+            cursor: pointer;
+        }
+    }
+}
+</style>
--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -3,6 +3,11 @@
         <template #left>
             <div class="content-container rounded-xl shadow-md p-4">
                 <CommonPostHeader></CommonPostHeader>
+                <div class="post-content">
+                    <h1>文章标题</h1>
+                    <p>文章内容</p>
+                </div>
+                <CommonTag></CommonTag>
             </div>
         </template>
         <template #right>
