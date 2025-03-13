0bbdfca2dae9e4a61c7881ee0820e17fc35bb0f0
Wrysmile <15035374951@126.com>
2025-03-13 23:26:37 +0800

    feat: 添加应用基础URL配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/NuxtBlog/'
+    },
     devtools: { enabled: true },
     devServer: {
         port: 5173,
